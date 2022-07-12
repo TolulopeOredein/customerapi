88a5aaa5a1b38a5ab1bcd328b2fe828bb42f9636
tolulopeoredein <oredeintolulope@gmail.com>
2022-07-12 12:53:00 +0100

    This is  a customer catalog Project. It consumes a REST API using Angular

---
+++ src/app/component/userdetail/userdetail.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { Response } from 'src/app/interface/response.interface';
+
+
+
+import { UserService } from 'src/app/service/user.service';
+
+@Component({
+  selector: 'app-userdetail',
+  templateUrl: './userdetail.component.html',
+  styleUrls: ['./userdetail.component.css']
+})
+export class UserdetailComponent implements OnInit {
+
+  response!: Response
+
+  mode :  'edit' | 'locked' = 'locked'
+  buttonText: string = 'Edit'
+  constructor(private activatedRoute: ActivatedRoute, private userService:UserService) { }
+
+  ngOnInit(): void {
+     this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+      console.log('User ID:', params.get('uuid')!);
+      this.userService.getUser(params.get('uuid')!).subscribe(
+        (response: any) => {
+          console.log(response);
+          this.response=response;
+        }
+      );
+      
+      
+
+    });
+       
+    // changeMode(mode?: 'edit' | 'locked') :void{
+
+    // }
+    
+  }
+
+}
